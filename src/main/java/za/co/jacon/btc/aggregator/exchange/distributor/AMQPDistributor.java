af95f8561d442aeb73235ef439882a80565e785d
Jaco Nel <jacon@rightshift.biz>
2014-12-11 15:00:08 +0200

    Start on the AMQP distributor.

--- src/main/java/za/co/jacon/btc/aggregator/exchange/distributor/AMQPDistributor.java
+++ src/main/java/za/co/jacon/btc/aggregator/exchange/distributor/AMQPDistributor.java
@@ -4,4 +4,22 @@ package za.co.jacon.btc.aggregator.exchange.distributor;
  * Distributes accumulated data via AMQP.
  */
 public class AMQPDistributor implements Distributor {
+
+    /**
+     * This method is responsible for distributing the dataObject for each exchange.
+     *
+     * Data is distributed onto a rabbitmq exchange.
+     *
+     * vhost: btca (for btc_aggregator)
+     * exhange: raw (for raw_data)
+     *
+     * the actual exchange name will be used as routing key.
+     *
+     * @param dataObject
+     */
+    public void distribute(Object dataObject, String exchange) {
+
+
+
+    }
 }
