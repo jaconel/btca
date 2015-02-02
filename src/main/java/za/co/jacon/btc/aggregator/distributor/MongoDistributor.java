854a9d10de21b5993758bb0822d74e9b03189df6
Jaco Nel <jacon@rightshift.biz>
2015-02-02 18:16:35 +0200

    Added empty files to form the base of additional distributors.

---
+++ src/main/java/za/co/jacon/btc/aggregator/distributor/MongoDistributor.java
@@ -0,0 +1,7 @@
+package za.co.jacon.btc.aggregator.distributor;
+
+/**
+ * The Mongodb distributor. Used to store statistics in the database
+ */
+public class MongoDistributor {
+}
---
+++ src/main/java/za/co/jacon/btc/aggregator/distributor/StatsdDistributor.java
@@ -0,0 +1,9 @@
+package za.co.jacon.btc.aggregator.distributor;
+
+/**
+ * Statsd distributor.
+ *
+ * Distributes information to a statsd endpoint.
+ */
+public class StatsdDistributor {
+}
